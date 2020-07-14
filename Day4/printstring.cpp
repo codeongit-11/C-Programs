ab7678c61f5b5e6befd75910ace20688fd2e054f
Shreya Sakshi <55545323+codeongit-11@users.noreply.github.com>
2020-07-14 19:27:01 +0530

    day 4 problems

---
+++ Day4/printstring.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+ 
+int main()
+{
+        char str[100];
+        cout<<"\nEnter any String : ";
+        cin>>str;
+        cout<<"\nThe Entered String is : "<<str<<endl;
+        return 0;
+}
